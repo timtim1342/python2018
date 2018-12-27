c9fc65dbfaa65d4a4dc7d02d1541ff8c2af889e8
timtim1342 <31654783+timtim1342@users.noreply.github.com>
2018-12-27 16:12:43 +0300

    Update hw5.py

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,23 +1,15 @@
 import sqlite3
 import re
+import os
 from flask import Flask, render_template, request
 
-
-app = Flask(__name__)
-@app.route('/')
-def index():
-    if request.args:
-        word = str(request.args['word'])
-        cont, url = search(word)
-        return render_template('results.html', z=range(len(cont)), url=url, cont=cont)
-    return render_template('index.html')
-
-def mktable():
+def mkdb():
     with open('vechorka/metadata.csv', 'r', encoding='utf-16') as f:
         txt = f.read()
     txtlst = txt.split('\n')
-    with open('results.csv', 'w', encoding='utf-16') as f:
-        pass
+    conn = sqlite3.connect('vechorka.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS sites(url text, author text, year text, content text, mscontent text)')
     for i in range(len(txtlst)-1):
         txtlst[i] = txtlst[i].split('\t')
         date = txtlst[i][3]
@@ -27,41 +19,46 @@ def mktable():
         with open(path, 'r', encoding='utf-8') as f:
             text = f.read()
             text = (re.sub('@.+','', text)).replace('\t', '').replace('\n', '')
-        with open('results.csv', 'a', encoding='utf-16') as f:
-            f.write(url + '\t' + author + '\t' + date + '\t' + text + '\n')
-
-def mkdb():
-    with open('results.csv', 'r', encoding='utf-16') as f:
-        txt = f.read()
-    txtlst = txt.split('\n')
-    conn = sqlite3.connect('vechorka.db')
-    c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS sites(url text, author text, year text, content text)')
-    for i in range(len(txtlst)-1):
-        txtlst[i] = txtlst[i].split('\t')
-        date = txtlst[i][2]
-        author = txtlst[i][1]
-        url = txtlst[i][0]
-        text = txtlst[i][3]
-        c.execute('INSERT INTO sites VALUES (?, ?, ?, ?)', (url, author, date, text))
+        path = path.replace('plain', 'mystem-plain')
+        with open(path, 'r', encoding='utf-8') as f:
+            mstext = f.read()
+            mstext = (re.sub('@.+','', mstext)).replace('\t', '').replace('\n', '')
+        c.execute('INSERT INTO sites VALUES (?, ?, ?, ?, ?)', (url, author, date, text, mstext))
         conn.commit()
     conn.close()
 
 def search(word):
-    word = '\'% ' + word + '%\''
+    word = '\'%{' + word + '%\''
     urls = []
     conts = []
     conn = sqlite3.connect('vechorka.db')
     c = conn.cursor()
-    for row in c.execute('SELECT url, content FROM sites WHERE content LIKE ' + word):
+    for row in c.execute('SELECT url, content FROM sites WHERE mscontent LIKE ' + word):
         urls.append(row[0])
         conts.append(row[1][0:200])
     return conts, urls
 
-def main():
-    mktable()
-    mkdb()
+app = Flask(__name__)
+@app.route('/')
+def index():
+    #mystemом начальная форма запрошенного слова
+    #сто проц можно сразу через консоль, но у меня он бычится
+    if request.args:
+        word = str(request.args['word'])
+        if ' ' not in word:
+            with open('word.txt', 'w', encoding='utf-8') as f:
+                f.write(word)
+            os.system('C:\\Users\\Tim\\mystem.exe ' + '-l ' + os.getcwd() + '\\' + 'word.txt '  + os.getcwd() + '\\' +
+                      'msword.txt')
+            with open('msword.txt', 'r', encoding='utf-8') as f:
+                txt = f.read()
+            word = re.findall(r'{(\w+)', txt)[0]
+            #дальше, вроде, все нормально
+            
+        cont, url = search(word)
+        return render_template('results.html', z=range(len(cont)), url=url, cont=cont)
+    return render_template('index.html')
                  
 if __name__ == '__main__':
-    main()
+    mkdb()
     app.run(debug=False)
